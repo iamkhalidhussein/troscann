e05a0bb95a0382762dcaad4b6abaf0c7208870e0
khalid09dev <mdkhalidhossen@hotmail.com>
2025-08-08 20:56:16 +0600

    feat: added hamberger icon

---
+++ app/components/icons/hamberger-menu.tsx
@@ -0,0 +1,13 @@
+export const HambergerMenu = () => {
+    return (
+        <svg
+            className="w-6 h-6"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+        >
+            <rect x="4" y="7" width="16" height="2" fill="currentColor" />
+            <rect x="4" y="15" width="16" height="2" fill="currentColor" />
+        </svg>
+    )
+};
