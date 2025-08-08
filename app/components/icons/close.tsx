722988a7f0f3a9f97ce4c7860caa627a714e8d2a
khalid09dev <mdkhalidhossen@hotmail.com>
2025-08-08 20:57:09 +0600

    feat: added close icon

---
+++ app/components/icons/close.tsx
@@ -0,0 +1,14 @@
+export const Close = () => {
+    return (
+        <svg
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+        >
+            <line x1="6" y1="6" x2="18" y2="18" stroke="currentColor" strokeWidth="2" />
+            <line x1="6" y1="18" x2="18" y2="6" stroke="currentColor" strokeWidth="2" />
+        </svg>
+    )
+};
